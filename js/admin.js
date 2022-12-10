dca6c041993a169fcb7337aa70239a7e9c4cbb39
siwere66087 <114493677+siwere66087@users.noreply.github.com>
2022-12-10 13:20:57 +0200

    Add files via upload

--- js/admin.js
+++ js/admin.js
@@ -128,22 +128,24 @@ JSON.parse(localStorage.getItem('products ')) :
 
 
 
-async function diplayprod(){
-    products.forEach((products)=> {
-        document.querySelector(".container").innerHTML +=
-            `<div class="container ">
-          <div class="card" style="width: 18rem;">
-        <img src="${products.pImage}" class="card-img-top img-thumbnail" style="height: 25rem; width:18rem;" >
-        <div class="card-body">
-        <h5 class="card-title">${products.pName}</h5>
-     <p class="card-text">${products.pPrice}</p>
-    <a href="#" class="btn btn-dark">Add to checkout</a>
-  </div>
-        </div>
-      </div>`;
-    })
+const price = [450, 360, 450, 200, 169, 229, 299, 275, 189, 129];
+
+
+
+
+async function tableDisplay() {
+  products.forEach((products) => {
+    document.querySelector("#tdata").innerHTML += `
+          <tr>
+            <th scope="row">${products.id}</th>
+            <td>${products.pName}</td>
+            <td>${products.isnb}</td>
+            <td>${products.pPrice}</td>
+          </tr>
+        `;
+  });
 }
-diplayprod();
+tableDisplay();
 
 
 
@@ -179,98 +181,110 @@ diplayprod();
 
 
 
-var selectedRow = null
-
-function onFormSubmit(e) {
-	e.preventDefault();
-        var formData = readFormData();
-        if (selectedRow == null){
-            insertNewRecord(formData);
-		}
-        else{
-            updateRecord(formData);
-		}
-        resetForm();    
-}
-
-//Retrieve the data
-function readFormData() {
-    var formData = {};
-    formData["productId"] = document.getElementById("productId").value;
-    formData["product"] = document.getElementById("product").value;
-    formData["quantity"] = document.getElementById("quantity").value;
-    formData["perPrice"] = document.getElementById("perPrice").value;
-    return formData;
-}
-
-//Insert the data
-function insertNewRecord(data) {
-    var table = document.getElementById("prodList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-		cell1.innerHTML = data.productId;
-    cell2 = newRow.insertCell(1);
-		cell2.innerHTML = data.product;
-    cell3 = newRow.insertCell(2);
-		cell3.innerHTML = data.quantity;
-    cell4 = newRow.insertCell(3);
-		cell4.innerHTML = data.perPrice;
-    cell4 = newRow.insertCell(4);
-        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
-}
-
-//Edit the data
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("productId").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("product").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("quantity").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
-}
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.productId;
-    selectedRow.cells[1].innerHTML = formData.product;
-    selectedRow.cells[2].innerHTML = formData.quantity;
-    selectedRow.cells[3].innerHTML = formData.perPrice;
-}
-
-//Delete the data
-function onDelete(td) {
-    if (confirm('you are about to delete this product!! do you want to continue?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById('prodList').deleteRow(row.rowIndex);
-        resetForm();
-    }
-}
-
-//Reset the data
-function resetForm() {
-    document.getElementById("productId").value = '';
-    document.getElementById("product").value = '';
-    document.getElementById("quantity").value = '';
-    document.getElementById("perPrice").value = '';
-    selectedRow = null;
-}
 
 
 
 
-async function diplayprod(){
-    products.forEach((products)=> {
-        document.querySelector(".container").innerHTML +=
-            `<div class="container ">
-          <div class="card" style="width: 18rem;">
-        <img src="${products.pImage}" class="card-img-top img-thumbnail" style="height: 25rem; width:18rem;" >
-        <div class="card-body">
-        <h5 class="card-title">${products.pName}</h5>
-     <p class="card-text">${products.pPrice}</p>
-    <a href="#" class="btn btn-dark">Add to checkout</a>
-  </div>
-        </div>
-      </div>`;
-    })
-}
-diplayprod();
+
+
+
+
+
+
+
+
+// var selectedRow = null
+
+// function onFormSubmit(e) {
+// 	e.preventDefault();
+//         var formData = readFormData();
+//         if (selectedRow == null){
+//             insertNewRecord(formData);
+// 		}
+//         else{
+//             updateRecord(formData);
+// 		}
+//         resetForm();    
+// }
+
+// //Retrieve the data
+// function readFormData() {
+//     var formData = {};
+//     formData["productId"] = document.getElementById("productId").value;
+//     formData["product"] = document.getElementById("product").value;
+//     formData["quantity"] = document.getElementById("quantity").value;
+//     formData["perPrice"] = document.getElementById("perPrice").value;
+//     return formData;
+// }
+
+// //Insert the data
+// function insertNewRecord(data) {
+//     var table = document.getElementById("prodList").getElementsByTagName('tbody')[0];
+//     var newRow = table.insertRow(table.length);
+//     cell1 = newRow.insertCell(0);
+// 		cell1.innerHTML = data.productId;
+//     cell2 = newRow.insertCell(1);
+// 		cell2.innerHTML = data.product;
+//     cell3 = newRow.insertCell(2);
+// 		cell3.innerHTML = data.quantity;
+//     cell4 = newRow.insertCell(3);
+// 		cell4.innerHTML = data.perPrice;
+//     cell4 = newRow.insertCell(4);
+//         cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+// }
+
+// //Edit the data
+// function onEdit(td) {
+//     selectedRow = td.parentElement.parentElement;
+//     document.getElementById("productId").value = selectedRow.cells[0].innerHTML;
+//     document.getElementById("product").value = selectedRow.cells[1].innerHTML;
+//     document.getElementById("quantity").value = selectedRow.cells[2].innerHTML;
+//     document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+// }
+// function updateRecord(formData) {
+//     selectedRow.cells[0].innerHTML = formData.productId;
+//     selectedRow.cells[1].innerHTML = formData.product;
+//     selectedRow.cells[2].innerHTML = formData.quantity;
+//     selectedRow.cells[3].innerHTML = formData.perPrice;
+// }
+
+// //Delete the data
+// function onDelete(td) {
+//     if (confirm('you are about to delete this product!! do you want to continue?')) {
+//         row = td.parentElement.parentElement;
+//         document.getElementById('prodList').deleteRow(row.rowIndex);
+//         resetForm();
+//     }
+// }
+
+// //Reset the data
+// function resetForm() {
+//     document.getElementById("productId").value = '';
+//     document.getElementById("product").value = '';
+//     document.getElementById("quantity").value = '';
+//     document.getElementById("perPrice").value = '';
+//     selectedRow = null;
+// }
+
+
+
+
+// async function diplayprod(){
+//     products.forEach((products)=> {
+//         document.querySelector(".container").innerHTML +=
+//             `<div class="container ">
+//           <div class="card" style="width: 18rem;">
+//         <img src="${products.pImage}" class="card-img-top img-thumbnail" style="height: 25rem; width:18rem;" >
+//         <div class="card-body">
+//         <h5 class="card-title">${products.pName}</h5>
+//      <p class="card-text">${products.pPrice}</p>
+//     <a href="#" class="btn btn-dark">Add to checkout</a>
+//   </div>
+//         </div>
+//       </div>`;
+//     })
+// }
+// diplayprod();
 
 
 

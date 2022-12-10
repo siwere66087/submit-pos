23878e0927efe01e288d93588d58393c741e028c
siwere66087 <114493677+siwere66087@users.noreply.github.com>
2022-12-10 15:31:35 +0200

    Add files via upload

--- js/admin.js
+++ js/admin.js
@@ -8,19 +8,17 @@ JSON.parse(localStorage.getItem('products ')) :
         
         id:1,
         pname:"cat shoe",
-        pdescription:"cat sneaker",
         pPrice:"R2110",
         pImage:"https://i.postimg.cc/GhVsXqp5/cat1.jpg",
-        count:1
+        quantity:1
     },
 
     {
         id:2,
         pname:"cat sneaker",
-        pdescription:"jogging sneaker",
         pPrice:"R1710",
         pImage:"https://i.postimg.cc/PJL1qcx1/cat2.webp",
-        count:1
+        quantity:1
     
     
     
@@ -29,10 +27,10 @@ JSON.parse(localStorage.getItem('products ')) :
       {
         id:3,
         pname:"sneaker",
-        pdescription:"formal shoe",
+        description:"formal shoe",
         pPrice:"R1110",
         pImage:"https://i.postimg.cc/D0bbhXjg/cat3.jpg",
-        count:1
+        quantity:1
     
     
     
@@ -40,10 +38,10 @@ JSON.parse(localStorage.getItem('products ')) :
      {
         id:4,
         pname:"cat shoe",
-        pdescription:"casual shoe",
+        description:"casual shoe",
         pPrice:"R810",
         pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
-        count:1
+        quantity:1
     
     
     
@@ -51,10 +49,10 @@ JSON.parse(localStorage.getItem('products ')) :
     {
         id:4,
         pname:"cat shoe",
-        pdescription:"casual shoe",
+        description:"casual shoe",
         pPrice:"R810",
         pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
-        count:1
+        quantity:1
     
     
     
@@ -62,10 +60,10 @@ JSON.parse(localStorage.getItem('products ')) :
     {
         id:4,
         pname:"cat shoe",
-        pdescription:"casual shoe",
+        description:"casual shoe",
         pPrice:"R810",
         pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
-        count:1
+        quantity:1
     
     
     
@@ -73,10 +71,10 @@ JSON.parse(localStorage.getItem('products ')) :
     {
         id:4,
         pname:"cat shoe",
-        pdescription:"casual shoe",
+        description:"casual shoe",
         pPrice:"R810",
         pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
-        count:1
+        quantity:1
     
     
     
@@ -84,10 +82,10 @@ JSON.parse(localStorage.getItem('products ')) :
     {
         id:4,
         pname:"cat shoe",
-        pdescription:"casual shoe",
+        description:"casual shoe",
         pPrice:"R810",
         pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
-        count:1
+        quantity:1
     
     
     
@@ -95,10 +93,10 @@ JSON.parse(localStorage.getItem('products ')) :
     {
         id:4,
         pname:"cat shoe",
-        pdescription:"casual shoe",
+        description:"casual shoe",
         pPrice:"R810",
         pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
-        count:1
+        quantity:1
     
     
     
@@ -106,20 +104,20 @@ JSON.parse(localStorage.getItem('products ')) :
     {
         id:4,
         pname:"cat shoe",
-        pdescription:"casual shoe",
+        description:"casual shoe",
         pPrice:"R810",
         pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
-        count:1
+        quantity:1
     
     
     },
     {
         id:4,
         pname:"cat shoe",
-        pdescription:"casual shoe",
+        description:"casual shoe",
         pPrice:"R810",
         pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
-        count:1
+        quantity:1
     
     
     },
@@ -127,8 +125,6 @@ JSON.parse(localStorage.getItem('products ')) :
 
 
 
-
-const price = [450, 360, 450, 200, 169, 229, 299, 275, 189, 129];
 
 
 
@@ -138,8 +134,8 @@ async function tableDisplay() {
     document.querySelector("#tdata").innerHTML += `
           <tr>
             <th scope="row">${products.id}</th>
-            <td>${products.pName}</td>
-            <td>${products.isnb}</td>
+            <td>${products.pname}</td>
+            <td>${products.description}</td>
             <td>${products.pPrice}</td>
           </tr>
         `;
@@ -193,98 +189,82 @@ tableDisplay();
 
 
 
-// var selectedRow = null
+let selectedRow = null
 
-// function onFormSubmit(e) {
-// 	e.preventDefault();
-//         var formData = readFormData();
-//         if (selectedRow == null){
-//             insertNewRecord(formData);
-// 		}
-//         else{
-//             updateRecord(formData);
-// 		}
-//         resetForm();    
-// }
+function onFormSubmit(e) {
+	e.preventDefault();
+        var formData = readFormData();
+        if (selectedRow == null){
+            insertNewRecord(formData);
+		}
+        else{
+            updateRecord(formData);
+		}
+        resetForm();    
+}
 
-// //Retrieve the data
-// function readFormData() {
-//     var formData = {};
-//     formData["productId"] = document.getElementById("productId").value;
-//     formData["product"] = document.getElementById("product").value;
-//     formData["quantity"] = document.getElementById("quantity").value;
-//     formData["perPrice"] = document.getElementById("perPrice").value;
-//     return formData;
-// }
+//Retrieve the data
+function readFormData() {
+    var formData = {};
+    formData["productId"] = document.getElementById("productId").value;
+    formData["pname"] = document.getElementById("pname").value;
+    formData["description"] = document.getElementById("description").value;
+    formData["perPrice"] = document.getElementById("perPrice").value;
+    return formData;
+}
 
-// //Insert the data
-// function insertNewRecord(data) {
-//     var table = document.getElementById("prodList").getElementsByTagName('tbody')[0];
-//     var newRow = table.insertRow(table.length);
-//     cell1 = newRow.insertCell(0);
-// 		cell1.innerHTML = data.productId;
-//     cell2 = newRow.insertCell(1);
-// 		cell2.innerHTML = data.product;
-//     cell3 = newRow.insertCell(2);
-// 		cell3.innerHTML = data.quantity;
-//     cell4 = newRow.insertCell(3);
-// 		cell4.innerHTML = data.perPrice;
-//     cell4 = newRow.insertCell(4);
-//         cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
-// }
+//Insert the data
+function insertNewRecord(data) {
+    var table = document.getElementById("prodList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.productId;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.pname;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML = data.description;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.perPrice;
+    cell4 = newRow.insertCell(4);
+        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+}
 
-// //Edit the data
-// function onEdit(td) {
-//     selectedRow = td.parentElement.parentElement;
-//     document.getElementById("productId").value = selectedRow.cells[0].innerHTML;
-//     document.getElementById("product").value = selectedRow.cells[1].innerHTML;
-//     document.getElementById("quantity").value = selectedRow.cells[2].innerHTML;
-//     document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
-// }
-// function updateRecord(formData) {
-//     selectedRow.cells[0].innerHTML = formData.productId;
-//     selectedRow.cells[1].innerHTML = formData.product;
-//     selectedRow.cells[2].innerHTML = formData.quantity;
-//     selectedRow.cells[3].innerHTML = formData.perPrice;
-// }
+//Edit the data
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("productId").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("pname").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("description").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.productId;
+    selectedRow.cells[1].innerHTML = formData.pname;
+    selectedRow.cells[2].innerHTML = formData.description;
+    selectedRow.cells[3].innerHTML = formData.perPrice;
+}
 
-// //Delete the data
-// function onDelete(td) {
-//     if (confirm('you are about to delete this product!! do you want to continue?')) {
-//         row = td.parentElement.parentElement;
-//         document.getElementById('prodList').deleteRow(row.rowIndex);
-//         resetForm();
-//     }
-// }
+//Delete the data
+function onDelete(td) {
+    if (confirm('you are about to delete this product!! do you want to continue?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById('prodList').deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
 
-// //Reset the data
-// function resetForm() {
-//     document.getElementById("productId").value = '';
-//     document.getElementById("product").value = '';
-//     document.getElementById("quantity").value = '';
-//     document.getElementById("perPrice").value = '';
-//     selectedRow = null;
-// }
-
-
+//Reset the data
+function resetForm() {
+    document.getElementById("productId").value = '';
+    document.getElementById("pname").value = '';
+    document.getElementById("description").value = '';
+    document.getElementById("perPrice").value = '';
+    selectedRow = null;
+}
 
 
-// async function diplayprod(){
-//     products.forEach((products)=> {
-//         document.querySelector(".container").innerHTML +=
-//             `<div class="container ">
-//           <div class="card" style="width: 18rem;">
-//         <img src="${products.pImage}" class="card-img-top img-thumbnail" style="height: 25rem; width:18rem;" >
-//         <div class="card-body">
-//         <h5 class="card-title">${products.pName}</h5>
-//      <p class="card-text">${products.pPrice}</p>
-//     <a href="#" class="btn btn-dark">Add to checkout</a>
-//   </div>
-//         </div>
-//       </div>`;
-//     })
-// }
-// diplayprod();
+
+
 
 
 
--- js/checkout.js
+++ js/checkout.js
@@ -1,26 +1,190 @@
-function product(id, name, img, price) {
+
+
+
+
+
+let products = JSON.parse(localStorage.getItem('products ')) ?
+JSON.parse(localStorage.getItem('products ')) : 
+
+[
+    {
+        
+        id:1,
+        pname:"cat shoe",
+        pPrice:"R2110",
+        pImage:"https://i.postimg.cc/GhVsXqp5/cat1.jpg",
+        quantity:1
+    },
+
+    {
+        id:2,
+        pname:"cat sneaker",
+        pPrice:"R1710",
+        pImage:"https://i.postimg.cc/PJL1qcx1/cat2.webp",
+        quantity:1
+    
+    
+    
+     },
+
+      {
+        id:3,
+        pname:"sneaker",
+        description:"formal shoe",
+        pPrice:"R1110",
+        pImage:"https://i.postimg.cc/D0bbhXjg/cat3.jpg",
+        quantity:1
+    
+    
+    
+    },
+     {
+        id:4,
+        pname:"cat shoe",
+        description:"casual shoe",
+        pPrice:"R810",
+        pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
+        quantity:1
+    
+    
+    
+    },
+    {
+        id:4,
+        pname:"cat shoe",
+        description:"casual shoe",
+        pPrice:"R810",
+        pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
+        quantity:1
+    
+    
+    
+    },
+    {
+        id:4,
+        pname:"cat shoe",
+        description:"casual shoe",
+        pPrice:"R810",
+        pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
+        quantity:1
+    
+    
+    
+    },
+    {
+        id:4,
+        pname:"cat shoe",
+        description:"casual shoe",
+        pPrice:"R810",
+        pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
+        quantity:1
+    
+    
+    
+    },
+    {
+        id:4,
+        pname:"cat shoe",
+        description:"casual shoe",
+        pPrice:"R810",
+        pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
+        quantity:1
+    
+    
+    
+    },
+    {
+        id:4,
+        pname:"cat shoe",
+        description:"casual shoe",
+        pPrice:"R810",
+        pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
+        quantity:1
+    
+    
+    
+    },
+    {
+        id:4,
+        pname:"cat shoe",
+        description:"casual shoe",
+        pPrice:"R810",
+        pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
+        quantity:1
+    
+    
+    },
+    {
+        id:4,
+        pname:"cat shoe",
+        description:"casual shoe",
+        pPrice:"R810",
+        pImage:"https://i.postimg.cc/wBfVTD28/cat4.jpg",
+        quantity:1
+    
+    
+    },
+];
+
+
+
+function product(id, pname, pImage, pPrice) {
 	this.id = id;
-  this.name = name;
-  this.img = img;
-  this.price = price;
+  this.pname = pname;
+  this.pImage = pImage;
+  this.pPrice = pPrice;
 }
 
-const products = [
-  new product('pid1', 'Product 1', 'https://www.webdesignerdepot.com/cdn-origin/uploads/2008/11/sample-graphic.gif', 100),
-  new product('pid2', 'Product 2', 'https://www.webdesignerdepot.com/cdn-origin/uploads/2008/11/sample-graphic.gif', 100),
-  new product('pid3', 'Product 3', 'https://www.webdesignerdepot.com/cdn-origin/uploads/2008/11/sample-graphic.gif', 100)
-];
+
+  let myCartItem = localStorage.getItem("myCartItem");
+  myCartItem = JSON.parse(myCartItem);
+
+  if (myCartItem === null) {
+    console.log('Cart is Empty');
+  } else {
+    myCartItem.forEach(e => {
+        console.log(e);
+    })
+  }
+
+
+
+ let html = "<table border='1|1'>";
+ for (let i = 0; i < products.length; i++) {
+  html += "<th>" + "Image" + "</th>";
+  html += "<th>" + "Product Name" + "</th>";
+  html += "<th>" + "Price" + "</th>";
+  html += "<th>" + "quantity" + "</th>";
+  html += "<tr>";
+  html += "<td>" + '<img src="' + products[i].pImage + '">' + "</td>";
+  html += "<td>" +products[i].pname + "</td>";
+  html += "<td>" + "R" + products[i].pPrice + "</td>";
+
+  html += "</tr>";
+
+}
+html += "</table>";
+document.getElementById("cartSection").innerHTML = html;
+
+
+
+
+
+
+
+
+
 
 const container = $('.products');
 
 products.forEach((i) => {
 	container.append(`
   <div class="card">
-  	<img src="${i.img}" class="card-img-top" />
+  	<img src="${i.pImage}" class="card-img-top" />
     <div class="card-body">
-    	<div class="card-title">${i.name}</div>
-    	<p class="card-text">$${i.price}</p>
-      <footer><button class="btn btn-primary addtocart" data-id="${i.id}">Buy Now</button></footer>
+    	<div class="card-title">${i.pname}</div>
+    	<p class="card-text">$${i.pPrice}</p>
+      <button class="btn btn-primary addtocheckout" data-id="${i.id}">Buy Now</button>
     </div>
   </div>
   `);
@@ -36,26 +200,8 @@ function saveToCart(event) {
   if (product) {
   	// Save it to the localStorage cart
   } else {
-  	// The product wasn't found... maybe someone tampered with the HTML?
+  	alert ("The product wasn't found... ")?
   }
 }
 
 $('.addtocart').on('click', saveToCart);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
